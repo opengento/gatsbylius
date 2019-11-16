d078d9c0062e001e2d10b04968e8c6e3b2dc86f5
Pierre Martin <pierre@occitech.fr>
2019-11-16 18:22:28 +0100

    feat(gatsby): ajout de tous les produits sur la HP

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
@@ -15,7 +15,27 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+
+    <h2>Nos produits</h2>
+    <ul>
+      {data.allProduct.nodes.map(product => (
+        <li>
+          <Link to={`/product/${product.slug}`}>{product.name}</Link>
+        </li>
+      ))}
+    </ul>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query HomePageQuery {
+    allProduct {
+      nodes {
+        slug
+        name
+      }
+    }
+  }
+`
