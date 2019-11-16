c25be02acd16aef7909f3b058ee13fc364edd5a1
Julien Pradet <julien.pradet@gmail.com>
2019-11-16 15:31:08 +0100

    Implement a static list of products and create the associated pages

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -3,5 +3,88 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require(`path`)
+const productListResponse = require("./__fixtures__/product-latest.json")
 
-// You can delete this file if you're not using it
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode } = actions
+
+  const adaptProduct = originalProduct => {
+    return {
+      code: originalProduct.code,
+      slug: originalProduct.slug,
+      name: originalProduct.name,
+      description: originalProduct.description,
+    }
+  }
+
+  const createNodeFromProduct = productData => {
+    const nodeContent = JSON.stringify(productData)
+
+    const nodeMeta = {
+      id: createNodeId(`product-${productData.code}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: `Product`,
+        mediaType: `text/html`,
+        content: nodeContent,
+        contentDigest: createContentDigest(productData),
+      },
+    }
+
+    const node = Object.assign({}, productData, nodeMeta)
+    createNode(node)
+  }
+
+  // Data can come from anywhere, but for now create it manually
+  productListResponse.items.forEach(originalProductData => {
+    const productData = adaptProduct(originalProductData)
+    createNodeFromProduct(productData)
+  })
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/product.js`)
+  // Query for markdown nodes to use in creating pages.
+  // You can query for whatever data you want to create pages for e.g.
+  // products, portfolio items, landing pages, etc.
+  // Variables can be added as the second function parameter
+  return graphql(
+    `
+      query loadProductsQuery {
+        allProduct {
+          nodes {
+            code
+            slug
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    // Create product post pages.
+    result.data.allProduct.nodes.forEach(node => {
+      createPage({
+        // Path for this page — required
+        path: `/product/${node.slug}`,
+        component: blogPostTemplate,
+        context: {
+            slug: node.slug
+          // Add optional context data to be inserted
+          // as props into the page component..
+          //
+          // The context data can also be used as
+          // arguments to the page GraphQL query.
+          //
+          // The page "path" is always available as a GraphQL
+          // argument.
+        },
+      })
+    })
+  })
+}
