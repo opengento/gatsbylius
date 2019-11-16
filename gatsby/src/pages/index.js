59b3f847dfdf3dbe9844bd190c671a64e93b4dea
xavier <xavier@kiboko.fr>
2019-11-16 19:07:17 +0100

    Add categories in HP

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -9,6 +9,17 @@ const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hello world</h1>
+    <h2>Nos catégories</h2>
+    <ul>
+        {data.allCategory.edges.map(({ node }) => {
+            return (
+                <li key={node.code}>
+                    <Link to={`/categories/${node.code}`}>{node.name}</Link>
+                </li>
+            )
+        })}
+    </ul>
+
     <h2>Nos produits</h2>
     <ul>
       {data.allProduct.nodes.map(product => (
@@ -24,6 +35,16 @@ export default IndexPage
 
 export const query = graphql`
   query HomePageQuery {
+    allCategory {
+      edges {
+        node {
+          id
+          code
+          slug
+          name
+        }
+      }
+    }
     allProduct {
       nodes {
         slug
