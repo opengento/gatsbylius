3988ab914ba3d47e8e4f34b1bf2c1778a2e577a9
misnard <maher.isnard@gmail.com>
2019-11-18 19:07:20 +0100

    Add products to category template

--- gatsby/src/templates/category.js
+++ gatsby/src/templates/category.js
@@ -1,11 +1,34 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import Img from "gatsby-image"
 
 const Category = props => {
   return (
     <Layout>
       <h1>{props.data.category.name}</h1>
+      <p>Products</p>
+      <ul
+        style={{
+          listStyle: "none",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {props.data.category.fields &&
+          props.data.category.fields.products.map(product => {
+            return (
+              <li key={product.slug}>
+                <Link to={`product/${product.slug}`}>
+                  <Img fixed={product.localImage.childImageSharp.fixed} />{" "}
+                  <br />
+                  {product.name}
+                </Link>
+              </li>
+            )
+          })}
+      </ul>
       <ul>
         {props.data.allCategory.edges.map(({ node }) => {
           return (
@@ -27,6 +50,22 @@ export const query = graphql`
       code
       slug
       name
+      fields {
+        products {
+          id
+          name
+          slug
+          localImage {
+            childImageSharp {
+              # Specify the image processing specifications right in the query.
+              # Makes it trivial to update as your page's design changes.
+              fixed(width: 125, height: 125) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+        }
+      }
     }
 
     allCategory {
@@ -36,12 +75,6 @@ export const query = graphql`
           code
           slug
           name
-          fields {
-            products {
-              id
-              name
-            }
-          }
         }
       }
     }
