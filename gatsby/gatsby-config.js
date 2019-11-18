cf0ccc09612cb17b94d94ed0af1694bf62af6a1a
Pierre Martin <pierre@occitech.fr>
2019-11-18 09:57:49 +0100

    build: setup deployment on GH Pages

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -3,6 +3,7 @@ require("dotenv").config({
 })
 
 module.exports = {
+  pathPrefix: "/gatsbylius",
   siteMetadata: {
     title: `Gatsbylius`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
