0e692d1cefda25406c4bf2c9b0f7a4c184f92e8a
Pierre Martin <pierre@occitech.fr>
2019-11-22 09:41:00 +0100

    feat: improve wording and add contributors on the home page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -5,43 +5,97 @@ import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="The fastest shop on earth!" />
-    <h1>Hello world</h1>
+const IndexPage = ({ data }) => {
+  const [contributors, setContributors] = React.useState(null)
+  React.useEffect(() => {
+    fetch("https://api.github.com/repos/opengento/gatsbylius/contributors")
+      .then(response => response.json())
+      .then(setContributors)
+  }, [])
 
-    <h2>Nos produits</h2>
-    <ul
-      style={{
-        listStyle: "none",
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-      }}
-    >
-      {data.allProduct.nodes.map(product => (
-        <li key={product.slug}>
-          <Link to={`/product/${product.slug}`}>
-            <Img fixed={product.localImage.childImageSharp.fixed} />
-            <br />
-            {product.name}
-          </Link>
-        </li>
-      ))}
-    </ul>
+  return (
+    <Layout>
+      <SEO title="The fastest shop on earth!" />
+      <h1>You're browsing the fastest shop on earth!</h1>
+      <p>
+        This is an experimentation created at a friendly{" "}
+        <a href="https://opengento.fr/">Opengento</a> meetup in France, over a
+        weekend. It uses the{" "}
+        <a href="https://github.com/Sylius/ShopApiPlugin">Sylius Shop API</a> as
+        a <a href="https://www.gatsbyjs.org/">Gatsby</a> source, to create a
+        fast eCommerce PWA.
+      </p>
 
-    <h2>Nos catégories</h2>
-    <ul>
-      {data.allCategory.edges.map(({ node }) => {
-        return (
-          <li key={node.code}>
-            <Link to={`/categories/${node.code}`}>{node.name}</Link>
-          </li>
-        )
-      })}
-    </ul>
-  </Layout>
-)
+      <section>
+        <h2>Our products</h2>
+        <ul
+          style={{
+            listStyle: "none",
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+          }}
+        >
+          {data.allProduct.nodes.map(product => (
+            <li key={product.slug}>
+              <Link to={`/product/${product.slug}`}>
+                <Img fixed={product.localImage.childImageSharp.fixed} />
+                <br />
+                {product.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section>
+        <h2>Our categories</h2>
+        <ul>
+          {data.allCategory.edges.map(({ node }) => {
+            return (
+              <li key={node.code}>
+                <Link to={`/categories/${node.code}`}>{node.name}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      {contributors && (
+        <section>
+          <h2>Thanks!</h2>
+          <p>Thanks to all these contributors who worked on this project:</p>
+          <ul
+            style={{
+              listStyle: "none",
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
+          >
+            {contributors.map(contributor => (
+              <li key={contributor.id}>
+                <a href={contributor.html_url}>
+                  <img
+                    src={contributor.avatar_url}
+                    width="100px"
+                    alt={`Avatar of ${contributor.login}`}
+                  />
+                  <br />
+                  {contributor.login}
+                </a>
+                <br />
+                <a href={`https://github.com/opengento/gatsbylius/commits?author=${contributor.login}`} alt={`Contributions of ${contributor.login}`}>
+                  ({contributor.contributions} contributions)
+                  </a>
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
 
